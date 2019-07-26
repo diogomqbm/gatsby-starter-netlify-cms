e8a2783f50c06ef2e46953e2f4d0d1728adb4ccf
mafrinha <diogomqbm13@gmail.com>
2019-07-26 12:20:18 -0300

    uns trem ai

--- src/widgets/youtube.js
+++ src/widgets/youtube.js
@@ -1,28 +1,5 @@
 import CMS from 'netlify-cms';
 
-var CategoriesControl = createClass({
-  handleChange: function(e) {
-    this.props.onChange(e.target.value.split(',').map((e) => e.trim()));
-  },
-
-  render: function() {
-    var value = this.props.value;
-    return h('input', { type: 'text', value: value ? value.join(', ') : '', onChange: this.handleChange });
-  }
-});
-
-var CategoriesPreview = createClass({
-  render: function() {
-    return h('ul', {},
-      this.props.value.map(function(val, index) {
-        return h('li', {key: index}, val);
-      })
-    );
-  }
-});
-
-CMS.registerWidget('categories', CategoriesControl, CategoriesPreview);
-
 CMS.registerEditorComponent({
   // Internal id of the component
   id: "youtube",
